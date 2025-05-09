fbb87d3b71a251c69d53bc1a971fed3c0732a670
mmlumbwe <mathias.lumbwe@gmail.com>
2025-05-09 13:40:17 +0200

    work gt06 20250509_1340

--- src/main/java/com/assettrack/iot/protocol/Gt06Handler.java
+++ src/main/java/com/assettrack/iot/protocol/Gt06Handler.java
@@ -289,7 +289,7 @@ public class Gt06Handler implements ProtocolHandler {
 
             byte expectedChecksum = data[checksumIndex];
             byte calculatedChecksum = calculateDeviceChecksum(data, checksumStart, checksumEnd);
-s
+
             logger.info("Checksum calculation - bytes: {}", bytesToHex(Arrays.copyOfRange(data, payloadStart, checksumEnd + 1)));
             logger.info("Checksum - expected: 0x{}, calculated: 0x{}", String.format("%02X", expectedChecksum), String.format("%02X", calculatedChecksum));
 
