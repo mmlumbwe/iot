814f72ace63f00c873d99885f612c12086094521
mmlumbwe <mathias.lumbwe@gmail.com>
2025-05-19 18:54:39 +0200

    session management 20250519_1854

--- src/main/java/com/assettrack/iot/protocol/ProtocolDetectionHandler.java
+++ src/main/java/com/assettrack/iot/protocol/ProtocolDetectionHandler.java
@@ -1,9 +1,9 @@
 package com.assettrack.iot.protocol;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +14,6 @@ public class ProtocolDetectionHandler extends ChannelInboundHandlerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(ProtocolDetectionHandler.class);
     private final ProtocolDetector protocolDetector;
 
-
     @Autowired
     public ProtocolDetectionHandler(ProtocolDetector protocolDetector) {
         this.protocolDetector = protocolDetector;
@@ -22,40 +21,41 @@ public class ProtocolDetectionHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        if (msg == null) {
-            ctx.fireChannelRead(msg); // Pass null along the pipeline
-            return;
-        }
-
-        if (!(msg instanceof ByteBuf)) {
-            // Not a ByteBuf, pass to next handler
-            ctx.fireChannelRead(msg);
-            return;
-        }
-
-        ByteBuf buf = (ByteBuf) msg;
         try {
-            if (buf.readableBytes() > 0) {
-                byte[] data = new byte[buf.readableBytes()];
-                buf.getBytes(buf.readerIndex(), data);
+            if (msg == null) {
+                logger.info("Received null message, skipping processing");
+                return;
+            }
 
-                if (data.length < ProtocolDetector.MIN_DATA_LENGTH) {
-                    logger.debug("Received data too short: {} bytes", data.length);
-                    return;
-                }
+            if (!(msg instanceof ByteBuf)) {
+                logger.info("Received non-ByteBuf message of type {}, forwarding", msg.getClass().getName());
+                ctx.fireChannelRead(msg);
+                return;
+            }
 
-                ProtocolDetector.ProtocolDetectionResult result = protocolDetector.detect(data);
+            ByteBuf buf = (ByteBuf) msg;
+            if (!buf.isReadable()) {
+                logger.info("Received empty ByteBuf, skipping processing");
+                return;
+            }
+
+            byte[] data = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(), data);
+
+            ProtocolDetector.ProtocolDetectionResult result = protocolDetector.detect(data);
+            if (result != null) {
                 ctx.fireChannelRead(result);
             }
-            ctx.fireChannelRead(msg);
+        } catch (Exception e) {
+            logger.error("Error during protocol detection", e);
         } finally {
-            buf.release(); // Ensure ByteBuf is released
+            ReferenceCountUtil.release(msg);
         }
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        logger.error("Protocol detection error", cause);
+        logger.error("Protocol detection pipeline error", cause);
         ctx.close();
     }
 }
