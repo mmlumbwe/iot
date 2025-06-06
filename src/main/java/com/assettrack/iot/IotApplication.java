0b2e8e1ee55b1f3f1618343f8a3634fb41932d5b
mmlumbwe <mathias.lumbwe@gmail.com>
2025-06-06 18:44:25 +0200

    Teltonika on nettyserver 20250606_1844

--- src/main/java/com/assettrack/iot/IotApplication.java
+++ src/main/java/com/assettrack/iot/IotApplication.java
@@ -1,17 +1,10 @@
 package com.assettrack.iot;
 
-import com.assettrack.iot.protocol.ProtocolDetectionHandler;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.List;
-
-
 
 @SpringBootApplication
 @EnableAsync
@@ -25,17 +18,6 @@ public class IotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IotApplication.class, args);
-	}
-
-	@Autowired
-	private List<ProtocolDetectionHandler> allHandlers;
-
-	@PostConstruct
-	public void verifyBeans() {
-		System.out.println(">>>> Number of ProtocolDetectionHandler beans = " + allHandlers.size());
-		for (ProtocolDetectionHandler h : allHandlers) {
-			System.out.println(">>>> Handler ID: " + System.identityHashCode(h));
-		}
 	}
 
 }
